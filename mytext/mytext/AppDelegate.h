a7ec14a6a043e15bc00eb5e9bad215ef185c7f4a
小树普惠 <>
2017-10-17 14:51:44 +0800

    lwq

---
+++ mytext/mytext/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mytext
+//
+//  Created by 小树普惠 on 2017/10/17.
+//  Copyright © 2017年 lwq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mytext/mytext/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mytext
+//
+//  Created by 小树普惠 on 2017/10/17.
+//  Copyright © 2017年 lwq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
